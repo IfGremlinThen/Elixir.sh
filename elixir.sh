4e7e9c14e3abe1dd12026a3c74d5db3262c404aa
IfGremlinThen <81646252+IfGremlinThen@users.noreply.github.com>
2022-12-17 10:59:41 -0600

    Update elixir.sh
    Echo'd and colored all processes for transparency,
    Improved and duplicated autoremove to purge before and after upgrade,
    Script now also clears the Firefox cache and VLC thumbnails,
    Script now also removes .gz archives from /var/,
    Script now purges packages that were removed without purge.

--- elixir.sh
+++ elixir.sh
@@ -1,24 +1,53 @@
 #!/bin/sh
-echo "CLEARING SPACE..."
-  sync && echo 3 | sudo tee /proc/sys/vm/drop_caches #CLEAR DISC CACHE
-  journalctl --disk-usage && sudo journalctl --vacuum-time=3d #CHECKS & REMOVES SYSTEMD JOURNAL LOGS OLDER THAN 3 DAYS, LOCATED IN /VAR/LOG/JOURNAL
-  sudo rm -rf /var/lib/snapd/snaps/* #REMOVES OLDER VERSIONS OF SNAP PACKAGES
-  sudo rm -rf ~/.cache/thumbnails/* #REMOVES ENTIRE THUMBNAIL CACHE
-  rm -rf ~/.local/share/Trash/files/* #EMPTIES TRASH
-  
-echo "UPDATING PACKAGE CATALOG..."
-  sudo dpkg --configure -a #CONFIGURES ANY UNCONFIGURED & UNPACKED PACKAGES
-  sudo apt --fix-broken install #-y #SEARCHES FOR BROKEN PACKAGES
-  sudo apt update #UPDATES EXISTING REPOSITORIES
-  sudo apt upgrade -y #UPDATES PACKAGES FROM REPOSITORIES
+RED="\e[31m"
+GREEN="32"
+BOLDGREEN="\e[1;${GREEN}m"
+LIGHTBLUE="\e[94m"
+ENDCOLOR="\e[0m"
+
+echo -e "${LIGHTBLUE}CLEARING DISC CACHE...${ENDCOLOR}"
+  sync && echo 3 | sudo tee /proc/sys/vm/drop_caches
+echo -e "${LIGHTBLUE}CLEARING THUMBNAIL CACHE...${ENDCOLOR}"
+  sudo rm -rf ~/.cache/thumbnails/*
+
+echo -e "${LIGHTBLUE}REMOVING SYSTEMD JOURNAL LOGS OLDER THAN 3 DAYS...${ENDCOLOR}"
+  sudo journalctl --vacuum-time=3d
+echo -e "${LIGHTBLUE}REMOVING OLDER VERSIONS OF SNAP PACKAGES...${ENDCOLOR}"
+  sudo rm -rf /var/lib/snapd/snaps/*
+echo -e "${LIGHTBLUE}REMOVING BACKUP ARCHIVES FROM /VAR/...${ENDCOLOR}"
+  sudo rm -rf /var/backups/*.gz
+echo -e "${LIGHTBLUE}PURGING ORPHANED PACKAGES...${ENDCOLOR}"
+  sudo apt autoremove -y --purge
+
+echo -e "${LIGHTBLUE}CLEARING FIREFOX CACHE...${ENDCOLOR}"
+  cd ~/.cache/mozilla/firefox/*/cache2/entries/ && find -maxdepth 1 -name "*" -delete && cd
+echo -e "${LIGHTBLUE}CLEARING VLC THUMBNAILS...${ENDCOLOR}"
+  cd ~/.cache/vlc/art/arturl/ && find -maxdepth 1 -name "*" -delete && cd #CLEARS VLC ALBUM THUMBNAILS
+  cd ~/.cache/vlc/art/artistalbum/ && find -maxdepth 2 -name "*" -delete && cd #CLEARS VLC ALBUM THUMBNAILS
+
+echo -e "${LIGHTBLUE}EMPTYING THE TRASH...${ENDCOLOR}"
+rm -rf ~/.local/share/Trash/files/* #EMPTIES TRASH
+
+
+echo -e "${LIGHTBLUE}CONFIGURING UNCONFIGURED & UNPACKED PACKAGES...${ENDCOLOR}"
+  sudo dpkg --configure -a
+echo -e "${LIGHTBLUE}FIXING BROKEN PACKAGES...${ENDCOLOR}"
+  sudo apt --fix-broken install
+echo -e "${LIGHTBLUE}UPDATING REPOSITORIES...${ENDCOLOR}"
+  sudo apt update
+echo -e "${LIGHTBLUE}UPDGRADING PACKAGES...${ENDCOLOR}"
+  sudo apt upgrade -y
     #IF YOU RECEIVE "dpkg-deb: error: paste subprocess was killed by signal (Broken pipe)" THEN RUN "sudo dpkg -i --force-overwrite var/cache/apt/archives/[PACKAGE NAME]"
-  sudo apt autoremove -y #REMOVES OBSOLETE PACKAGES PREVIOUSLY INSTALLED AS DEPENDENCIES
-  sudo apt clean #REMOVES OBSOLETE PACKAGE CACHES & INSTALLERS
+echo -e "${LIGHTBLUE}PURGING NEWLY ORPHANED PACKAGES...${ENDCOLOR}"
+  sudo apt autoremove -y --purge
+echo -e "${LIGHTBLUE}REMOVING OBSOLETE PACKAGES & INSTALLERS...${ENDCOLOR}"
+  sudo apt clean
+echo -e "${LIGHTBLUE}REMOVING RESIDUAL CONFIGURATION FILES FROM REMOVED PACKAGES...${ENDCOLOR}"
+  sudo dpkg -l | grep '^rc' | awk '{print $2}' | sudo xargs dpkg --purge
+
+echo -e "${BOLDGREEN}CLEANING COMPLETE!${ENDCOLOR}"
 
 echo "Consider addressing these files:"
 cd && find -type f -exec du -Sh {} + | sort -rh | head -n 8
 echo "Check /.local/share/applications for redundant files."
 echo "Please restart your computer."
-
-#REMOVE RESIDUAL CONFIGURATION FILES FROM REMOVED PACKAGES
-#sudo dpkg -l | grep '^rc' | awk '{print $2}' | sudo xargs dpkg --purge
